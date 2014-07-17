6e3af1dfad3293e7617e2e158600e6a956a674e9
amicoleo <leonardo.amico@gmail.com>
2014-07-17 15:19:37 +0200

    Implemented vertical carousel radio

---
+++ src/ofxUICarouselRadio.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "ofxUIWidgetWithLabel.h"
+#include "ofxUILabel.h"
+#include "ofxUIDefines.h"
+#include "ofxUIToggle.h"
+
+class ofxUICarouselRadio : public ofxUIWidget
+{
+public:
+    ofxUICarouselRadio(string _name, vector<string> names, int _orientation, float w, float h, float x = 0, float y = 0, int _size = OFX_UI_FONT_SMALL);
+    void init(string _name, vector<string> names, int _orientation, float w, float h, float x = 0, float y = 0, int _size = OFX_UI_FONT_SMALL);
+    void setVisible(bool _visible);
+    void triggerSelf();
+    void triggerEvent(ofxUIWidget *child);
+	void setParent(ofxUIWidget *_parent);
+	string getActiveName();
+    void addLabelName(string labelName);
+
+
+protected:
+    string activeName;
+	int orientation;
+	vector<ofxUILabel *> labels;
+    ofxUILabel *activeLabel;
+
+    ofxUIButton *downButton;
+    ofxUIButton *upButton;
+    bool bUpButton, bDownButton;
+};
